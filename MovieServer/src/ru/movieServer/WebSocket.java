3d3e124f09e2f8418b1e58fa6b12804e43a98349
Alexander <astrgan@gmail.com>
2018-03-19 23:35:23 +1000

    18

--- MovieServer/src/ru/movieServer/WebSocket.java
+++ MovieServer/src/ru/movieServer/WebSocket.java
@@ -1,10 +1,18 @@
 package ru.movieServer;
 
 import javax.inject.Inject;
+import javax.websocket.OnMessage;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/ws")
 public class WebSocket {
 
 	@Inject DBConnection dbConnection;
+	
+	@OnMessage
+	public String onMessage(String msg) {
+		System.out.println(msg);
+		return "Hello from Server :)))";
+		
+	}
 }
