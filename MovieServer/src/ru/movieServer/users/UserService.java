996e5281ec840cc65a8b1ac03ae171ce3b96fab0
astrgan <astrgan@gmail.com>
2018-07-04 23:04:32 +1000

    check email

--- MovieServer/src/ru/movieServer/users/UserService.java
+++ MovieServer/src/ru/movieServer/users/UserService.java
@@ -1,6 +1,9 @@
 package ru.movieServer.users;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.ejb.EJB;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
@@ -11,6 +14,13 @@ import javax.ws.rs.PathParam;
 @Path("/users")
 public class UserService {
 	
+	final Pattern VALID_EMAIL_ADDRESS_REGEX = 
+		    Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+	boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(emailStr);
+        return matcher.find();
+	}
+	
 	@EJB
 	DBConnectionUsers DBCUser;
 
@@ -18,37 +28,6 @@ public class UserService {
 //	@Consumes(MediaType.TEXT_PLAIN)
 	@POST
 	public String UsersProcess (@PathParam("command") String command, @FormParam("username") String username, @FormParam("email") String email, @FormParam("password") String password) {
-		
-		System.out.println("UsersProcess");
-		
-		if(command.equals("add")) {
-			
-			System.out.println("add");
-			String status = DBCUser.addUser(username, email ,password);
-			
-			String response = "{\"status\":\"" + status +"\"}";
-			System.out.println(response);
-			return response;
-			
-			
-		}
-
-		if(command.equals("auth")) {
-			
-			String token = DBCUser.authUser(email, password);
-			String code;
-			if(token != null) code = "0";
-			else code = "1";
-			String response = "{"
-					+ "\"status\": " + code + ","
-					+ "\"token\":\""+ token + "\","
-					+ "\"message\":\"Неверный EMAIL или ПАРОЛЬ\""
-					+ "}";
-			System.out.println(response);
-			
-			return response;
-			
-		} 
 		
 		if(command.equals("chekToken")) {
 			String code = "1";
@@ -70,6 +49,45 @@ public class UserService {
 			String response = "{"
 					+ "\"status\": " + code + ","
 					+ "\"name\":\""+ name + "\""
+					+ "}";
+			return response;
+		}
+		
+		if(validate(email)) {
+			
+			if(command.equals("add")) {
+				
+				System.out.println("add");
+				String status = DBCUser.addUser(username, email ,password);
+				
+				String response = "{\"status\":\"" + status +"\"}";
+				System.out.println(response);
+				return response;
+				
+				
+			}
+
+			if(command.equals("auth")) {
+				
+				String token = DBCUser.authUser(email, password);
+				String code;
+				if(token != null) code = "0";
+				else code = "1";
+				String response = "{"
+						+ "\"status\": " + code + ","
+						+ "\"token\":\""+ token + "\","
+						+ "\"message\":\"Неверный EMAIL или ПАРОЛЬ\""
+						+ "}";
+				System.out.println(response);
+				
+				return response;
+				
+			} 
+			
+		}else {
+			String response = "{"
+					+ "\"status\": " + 2 + ","
+					+ "\"message\":\"Некоректный EMAIL\""
 					+ "}";
 			return response;
 		}
